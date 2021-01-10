db3c3dbf88bf91485357fa58c94d0324c0244853
Alberto Jimenez <alberto.est.jimenez@gmail.com>
2021-01-10 18:46:34 -0500

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 echo ">>> Building"
 npm run build
 npm run export
+
+rm -rf src/main/webapp/_next 
 cp -r out/* src/main/webapp/
+
 echo ">>> Packaging"
 mvn package
+
 echo ">>> Deploying"
 sudo cp ~/Jenkins-Maven-NPM/target/Jenkins.war /opt/wildfly/standalone/deployments/ 
