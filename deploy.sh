bf432dd7167a68abee2df3a2c73b1924aac90e46
Alberto Jimenez <alberto.est.jimenez@gmail.com>
2021-01-10 20:51:38 -0500

    script split

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,3 @@
-echo ">>> Building"
-npm run build
-npm run export
-
-rm -rf src/main/webapp/test/*
-mkdir src/main/webapp/test
-cp -r out/* src/main/webapp/test
-
 echo ">>> Packaging"
 mvn package
 
